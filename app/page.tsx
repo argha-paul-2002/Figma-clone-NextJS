3e864b9c19c39cfea572cd2c91bd3db811b4f325
Argha Paul <arghapaul002@gmail.com>
2024-02-13 00:57:09 +0530

    Basic Sidebars added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 "use client";
+import LeftSidebar from "@/components/LeftSidebar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
+import RightSidebar from "@/components/RightSidebar";
 
 export default function Page() {
   return (
     <main className="h-screen overflow-hidden">
       <Navbar />
       <section className="flex h-full flex-row">
+        <LeftSidebar />
         <Live />
+        <RightSidebar />
       </section>
     </main>
   );
