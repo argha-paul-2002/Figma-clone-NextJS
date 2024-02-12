71b9a4830212e5b9b97a0b5eccce98094636a4c3
Argha Paul <arghapaul002@gmail.com>
2024-02-13 00:04:55 +0530

    Navbar Added with Active Users

---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from 'react'
+import Image from "next/image";
+import { memo } from "react";
+
+import { navElements } from "@/constants";
+import { ActiveElement, NavbarProps } from "@/types/type";
+import Activeusers from './users/ActiveUsers';
+
+// import { Button } from "./ui/button";
+// import ShapesMenu from "./ShapesMenu";
+
+const Navbar = ({activeElement}: NavbarProps) => {
+  return (
+    <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
+      <Image src="/assets/logo.svg" alt="FigPro Logo" width={58} height={20} />
+      <Activeusers />
+    </nav>
+
+  )
+}
+
+export default memo(Navbar, (prevProps, nextProps) => prevProps.activeElement === nextProps.activeElement )
