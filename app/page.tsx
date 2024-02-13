b7f07107be6b2e3f2bd42b2334cd92b83f41ea39
Argha Paul <arghapaul002@gmail.com>
2024-02-13 23:35:24 +0530

    Toolbar and Shapes Menu added to Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,9 @@ import LeftSidebar from "@/components/LeftSidebar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSidebar from "@/components/RightSidebar";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { handleCanvasMouseDown, handleResize, initializeFabric } from "@/lib/canvas";
+import { ActiveElement } from "@/types/type";
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -14,6 +15,17 @@ export default function Page() {
   const isDrawing = useRef(false);
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<string | null>('rectangle');
+
+  const [activeElement, setActiveElement] = useState<ActiveElement>({
+    name: '',
+    value: '',
+    icon: '',
+  })
+
+  const handleActiveElement = (elem: ActiveElement) => {
+    setActiveElement(elem);
+    selectedShapeRef.current = elem?.value as string;
+  };
 
   useEffect(()=>{
     const canvas = initializeFabric({canvasRef, fabricRef});
@@ -34,7 +46,10 @@ export default function Page() {
 
   return (
     <main className="h-screen overflow-hidden">
-      <Navbar />
+      <Navbar
+        activeElement={activeElement}
+        handleActiveElement={handleActiveElement}         
+      />
       <section className="flex h-full flex-row">
         <LeftSidebar />
         <Live canvasRef={canvasRef} />
