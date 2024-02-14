9fac7f601d61470878c202219f4b05419517da2f
Argha Paul <arghapaul002@gmail.com>
2024-02-14 14:37:27 +0530

    Undo and Redo Feature implemented

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,14 @@ import RightSidebar from "@/components/RightSidebar";
 import { useEffect, useRef, useState } from "react";
 import { handleCanvasMouseDown, handleCanvasMouseUp, handleCanvasObjectModified, handleCanvaseMouseMove, handleResize, initializeFabric, renderCanvas } from "@/lib/canvas";
 import { ActiveElement } from "@/types/type";
-import { useMutation, useStorage } from "@/liveblocks.config";
+import { useMutation, useRedo, useStorage, useUndo } from "@/liveblocks.config";
 import { defaultNavElement } from "@/constants";
-import { handleDelete } from "@/lib/key-events";
+import { handleDelete, handleKeyDown } from "@/lib/key-events";
 
 export default function Page() {
+  const undo = useUndo();
+  const redo = useRedo();
+
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const fabricRef = useRef<fabric.Canvas | null>(null);
   const isDrawing = useRef(false);
@@ -82,7 +85,7 @@ export default function Page() {
   useEffect(()=>{
     const canvas = initializeFabric({canvasRef, fabricRef});
 
-    canvas.on('mouse:down', (options) => {
+    canvas.on('mouse:down', (options: any) => {
       handleCanvasMouseDown({
         options,
         canvas,
@@ -92,7 +95,7 @@ export default function Page() {
       })
     })
 
-    canvas.on('mouse:move', (options) => {
+    canvas.on('mouse:move', (options: any) => {
       handleCanvaseMouseMove({
         options,
         canvas,
@@ -103,7 +106,7 @@ export default function Page() {
       })
     })
 
-    canvas.on('mouse:up', (options) => {
+    canvas.on('mouse:up', () => {
       handleCanvasMouseUp({
         canvas,
         isDrawing,
@@ -115,7 +118,7 @@ export default function Page() {
       })
     })
 
-    canvas.on("object:modified", (options) => {
+    canvas.on("object:modified", (options: any) => {
       handleCanvasObjectModified({
         options,
         syncShapeInStorage      
@@ -124,6 +127,17 @@ export default function Page() {
 
     window.addEventListener("resize", () => {
       handleResize({ fabricRef });
+    });
+
+    window.addEventListener("keydown", (e: any) => {
+      handleKeyDown({
+        e,
+        canvas: fabricRef.current,
+        undo,
+        redo,
+        syncShapeInStorage,
+        deleteShapeFromStorage
+      })
     });
 
     return () => {
