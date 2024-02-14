7dbedda7142ce905b32f22900ea666c2325a8404
Argha Paul <arghapaul002@gmail.com>
2024-02-14 17:53:37 +0530

    Image upload feature added

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ import { ActiveElement } from "@/types/type";
 import { useMutation, useRedo, useStorage, useUndo } from "@/liveblocks.config";
 import { defaultNavElement } from "@/constants";
 import { handleDelete, handleKeyDown } from "@/lib/key-events";
+import { handleImageUpload } from "@/lib/shapes";
 
 export default function Page() {
   const undo = useUndo();
@@ -22,6 +23,7 @@ export default function Page() {
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<string | null>(null);
   const activeObjectRef = useRef<fabric.Object | null>(null);
+  const imageInputRef = useRef<HTMLInputElement>(null);
 
   // Canvas Objects live storage provided by Liveblocks
   const canvasObjects = useStorage((root)=>root.canvasObjects)
@@ -74,6 +76,14 @@ export default function Page() {
       case 'delete':
         handleDelete(fabricRef.current as any, deleteShapeFromStorage);
         setActiveElement(defaultNavElement);
+        break;
+      case 'image':
+        imageInputRef.current?.click();
+        isDrawing.current = false;
+
+        if(fabricRef.current){
+          fabricRef.current.isDrawingMode = false;
+        }
         break;
     
       default:
@@ -158,7 +168,17 @@ export default function Page() {
     <main className="h-screen overflow-hidden">
       <Navbar
         activeElement={activeElement}
-        handleActiveElement={handleActiveElement}         
+        handleActiveElement={handleActiveElement} 
+        imageInputRef={imageInputRef}
+        handleImageUpload={(e: any) => {
+          e.stopPropagation();
+          handleImageUpload({
+            file: e.target.files[0],
+            canvas: fabricRef as any,
+            shapeRef,
+            syncShapeInStorage
+          })
+        }}        
       />
       <section className="flex h-full flex-row">
         {/* Camera Objects are not Array, it is a Map type. So type conversion is needed */}
